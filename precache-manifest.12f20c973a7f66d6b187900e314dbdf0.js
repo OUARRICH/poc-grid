23b35166bd1c66143cc585fce0549fc877975336
said.ouarrich <said.ouarrich-ext@socgen.com>
2020-03-10 10:47:07 +0100

    Updates

---
+++ precache-manifest.12f20c973a7f66d6b187900e314dbdf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de8c2a0434d4913dcc5f835924c3c86",
+    "url": "/poc-grid/index.html"
+  },
+  {
+    "revision": "2b1eb58b9c4e9da40cb9",
+    "url": "/poc-grid/static/css/main.4bd5b39e.chunk.css"
+  },
+  {
+    "revision": "44e4daf1fe3df08b3496",
+    "url": "/poc-grid/static/js/2.fb406a45.chunk.js"
+  },
+  {
+    "revision": "3871d0c2cfeeca52c94e655dbfb5599b",
+    "url": "/poc-grid/static/js/2.fb406a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1eb58b9c4e9da40cb9",
+    "url": "/poc-grid/static/js/main.18063750.chunk.js"
+  },
+  {
+    "revision": "b7a929d089ae499425cd",
+    "url": "/poc-grid/static/js/runtime-main.63605d95.js"
+  }
+]);
